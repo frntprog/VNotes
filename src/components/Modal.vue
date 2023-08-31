1a8073623e31237ec7933203e8cfb22908130c63
frntprog <frntprog@gmail.com>
2023-08-31 14:02:15 +0300

    added animation

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,14 +1,25 @@
 <template>
   <Teleport to="#modal">
-    <div
-      v-if="isModalOpen"
-      class="fixed top-0 left-0 w-screen h-screen bg-black opacity-50 flex justify-center items-center"
+    <Transition
+      name="modal"
+      enter-active-class="duration-300 ease-out"
+      enter-from-class="transform opacity-0"
+      leave-active-class="duration-200 ease-in"
+      leave-to-class="transform opacity-0"
     >
-      <div ref="modal" class="relative bg-white px-12 py-24 rounded shadow-md">
-        <button @click="hide" class="absolute top-0 right-2">x</button>
-        Click outside to close it
+      <div
+        v-if="isModalOpen"
+        class="fixed top-0 left-0 w-screen h-screen bg-black opacity-50 flex justify-center items-center"
+      >
+        <div
+          ref="modal"
+          class="relative bg-white px-12 py-24 rounded shadow-md"
+        >
+          <button @click="hide" class="absolute top-0 right-2">x</button>
+          Click outside to close it
+        </div>
       </div>
-    </div>
+    </Transition>
   </Teleport>
 </template>
 
