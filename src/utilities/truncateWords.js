c6a406c6c78c437c9bf5a8f02c13a3d234abb1fc
frntprog <frntprog@gmail.com>
2023-09-04 17:58:41 +0300

    removed log

--- src/utilities/truncateWords.js
+++ src/utilities/truncateWords.js
@@ -1,5 +1,4 @@
 export default function truncateWords(str, maxLength) {
-  console.log(str.length, maxLength);
   if (str.length < maxLength) return str;
   return str.substring(0, maxLength) + "...";
 }
